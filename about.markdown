995ec671ba8447bdfd0b7d9d88607bce5bf1dc4b
Salai Vedha Viradhan <salaivv@gmail.com>
2023-01-30 00:45:23 +0530

    Initiated Jekyll for code reuse

---
+++ about.markdown
@@ -0,0 +1,7 @@
+---
+title: About
+layout: page
+permalink: /about/
+---
+
+A Hacker News client (PWA) using Jekyll and Alpine.js.
---
+++ assets/main.js
@@ -0,0 +1,60 @@
+const BASE_URL = 'https://api.hnpwa.com/v0'
+const BASE_DOMAIN = 'https://news.ycombinator.com'
+
+const getItem = async (id) => {
+    const res = await fetch(`${BASE_URL}/item/${id}.json`)
+    const item = await res.json()
+    return item
+}
+
+const getURLParam = (url, param) => {
+    const params = new URLSearchParams(document.location.search);
+    return params.get(param); // is the string "Jonathan"
+}
+
+const getTopStories = async (page=1) => {
+    res = await fetch(`${BASE_URL}/news/${page}.json`)
+    stories = await res.json()
+
+    console.log(stories);
+    return stories
+}
+
+const getUserLink = (user) => {
+    return `${BASE_DOMAIN}/user?id=${user}`
+}
+
+const getItemLink = (id) => {
+    return `/item?id=${id}.json`
+}
+
+const getComments = (obj) => {
+    comments = []
+    const flattenComments = (obj) => {
+        Object.keys(obj).forEach(key => {        
+            if (key === 'comments') {
+                const comment = {
+                    id: obj.id, 
+                    user: obj.user,
+                    level: obj.level,
+                    time_ago: obj.time_ago,
+                    content: obj.content,
+                    url: obj.url
+                }
+                // console.log(comment);
+                comments.push(comment)
+            }
+
+            if (typeof obj[key] === 'object' && obj[key] !== null) {
+                flattenComments(obj[key])
+            }
+        })
+
+    }
+
+    flattenComments(obj)
+
+    console.log(comments.length);
+    console.log(comments);
+    return comments
+}
